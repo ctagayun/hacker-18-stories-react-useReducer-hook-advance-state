66a4ae27189d8916e25b61022106e05d01284f19
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-01-29 08:03:20 -0600

    Added more comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 /*================================================================
- React Reducer Hook
+ React Advance State Reducer Hook
+   Task: 
+
+    1. In this section, we will move the stateful 'STORIES' from React's 
+  useState hook to React's useReducer hook. In this section, we will start  
+  to manage the "stories and its state transitions "in a reducer. Reducer
+  enables sophisticated state management.
+    
+    2. Use reducer to manage state if there are multiple states related
+  to one domain that are related to each other. For example:
+       - stories (see )
+       - isLogging
+       - isError
+  are all related to the "data fetching"
+
      Basically reducers are there to manage state in an application. 
   For instance, if a user writes something in an HTML input field, the 
   application has to manage this UI state (e.g. controlled components).
 
-     In essence, a reducer is a function which takes two arguments:
-   the current state and an action -- and returns a new state based on both 
-   arguments state.
+    In essence, a reducer is a function which takes two arguments:
+       - the current state  
+       - an action 
+   and returns a new state based on both arguments values.
 
      Using useReducer over useState makes sense when  multiple stateful 
   values are dependent on each other or related to one domain. For example, 
@@ -14,13 +29,10 @@
   In all three (e.g. data, isLoading, error) could be properties in one 
   complex object managed by a reducer instead.
   
-  Task: 
-    In this section, we will move the stateful stories from React's 
-  useState hook to React's useReducer hook. In this section, we will start  
-  to manage the "stories and its state transitions "in a reducer.  
-   
-  Hints on how to accomplish this:
-    - First, introduce a reducer function outside of your components. 
+  
+  Hints on how to accomplish Task:
+    - First, introduce a reducer function OUTSIDE of your components and 
+    call it stories reducer. 
     A reducer function always receives a state and an action. Based 
     on these two arguments, a reducer always returns a new state:
 
@@ -61,24 +73,64 @@ const initialStories = [
   },
 ];
 
+/*Asynchronous data. We start off by writing getAsyncStories a function that returns 
+  a "Promise" with a data.
+  The resolve object holds the previous list of stories
+*/
 const getAsyncStories = () =>
   new Promise((resolve) =>
     setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
+      () => resolve({ data: { stories: initialStories } }), 
       2000
     )
   );
 
-const storiesReducer = (state, action) => {
-  switch (action.type) {
-    case 'SET_STORIES':
-      return action.payload;
-    case 'REMOVE_STORY':
-      return state.filter(
-        (story) => action.payload.objectID !== story.objectID
+/*
+  First, introduce a reducer function outside of your components. 
+  A reducer function always receives a state and an action (A). 
+  Based on these two arguments, a reducer always returns a new state:
+    1. 'action' is always associated with a type (B) and payload (B)
+    2. If the type matches a condition in the reducer (B), return a new state (C) 
+      based on incoming state and action.
+  */
+
+  //In the following example: 
+  //The storiesReducer function covers "one type" and then returns the 
+  //payload of the incoming action without using the current state to 
+  //compute the new state. The new state is therefore simply the payload.
+
+  //   const storiesReducer = (state, action) => {
+  //     if (action.type === 'SET_STORIES') {
+  //       return action.payload; //the new state is simply the payload
+  //     } else {
+  //       throw new Error();
+  //     }
+  //   };
+ 
+ //Define a reducer function. It always receives state and action
+ //   1.'action' is always associated with a type. In this example
+ //     there are 2 types: 'SET_STORIES' and 'REMOVE_STORY' 
+ //     each type returns appropriate action.payload.
+ //
+ //   2. If the type matches a condition  in this case 'SET_STORIES' 
+ //      and 'REMOVE_STORY' the reducer return a new state based on 
+ //       incoming state and action.
+ //   3. Based on these two arguments the reducer always returns 
+ //     a new state
+ 
+ const storiesReducer = (state, action) => { // A
+  switch (action.type) {  //(B)
+    case 'SET_STORIES':   
+      return action.payload;  //return a new state
+    case 'REMOVE_STORY':  //(C)
+      return state.filter(   //return a new state
+                             //but filter the state first. In this case objectID. 
+                             //Filter generates 
+                             //a new array from the 'state' an call it "story"
+        (story) => action.payload.objectID !== story.objectID //C
       );
     default:
-      throw new Error();
+      throw new Error(); //Error! action type is not  SET_STORIES or REMOVE_STORY
   }
 };
 
@@ -100,27 +152,73 @@ const App = () => {
     'React'
   );
 
+  /*
+    Create a variables to manage state using React.useState hook:
+
+    By using useState, we are telling React that we want to 
+    have a stateful value which changes over time.  
+    Since we haven't fetch the data yet, initialize the state 
+    'stories'with empty array and simulate fetching these stories 
+    async.
+    const [stories, setStories] = React.useState([]); (C)
+  /*
+
+  /* Replace above React.useState with React.useReducer hook.
+     The hook receives a reducer function storiesReducer (A) 
+     and initial state (B) as arguments.
+     It and returns an array with two items. 
+       1. The first item is the current state 
+       2  The second item is the state updater function 
+          (also called dispatch function):
+    The new dispatch function can be used instead of the 
+    setStories (C) function , which was previously returned 
+    from useState version of the hook.
+  */
+
+  //This initializes or setup the initial state of "stories" to an empty list
+  //Refactor the following to use React.useReducer hook. We want an empty list 
+  //of stories "[]" for the initial state
+ 
+  /*const [stories, setStories] = React.useState([]); */
+
+  //Same as in React.useState, the reducer initializes "stories" with an empty list
   const [stories, dispatchStories] = React.useReducer(
-    storiesReducer,
-    []
-  );
+      storiesReducer, //this the reducer function defined above.  
+      []  //We want an empty list [] for the initial state.  
+    );
+    //Instead of setting the state explicitly (like what the 
+    //useState updater function 'setStories' does) the
+    //updater function from , the useReducer  update  
+    //function called 'dispatchStories' sets the state implicitly 
+    //by dispatching an action for the reducer. See 
+    //getAsyncStories() below. 
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
+  //In the useEffect hook we call getAsyncStories a function that 
+  //returns "Promise" with a data (see line 80) and resolve the returned
+  //promisea as a side-effect.
+  //Due to the empty dependency array, the side-effect only runs once 
+  //the component renders for the first time:
   React.useEffect(() => {
     setIsLoading(true);
-
     getAsyncStories()
       .then((result) => {
-        dispatchStories({
-          type: 'SET_STORIES',
-          payload: result.data.stories,
+        dispatchStories({ // (A) Updater function sets state implicitly
+                          //by dispatching an action for the reducer it takes 2 args.
+          type: 'SET_STORIES',  //  type
+          payload: result.data.stories, //'action'
         });
         setIsLoading(false);
       })
       .catch(() => setIsError(true));
   }, []);
 
+  //This handler computes the new stories. It is the second state transition.
+  //It is valid to move this logic into the reducer function. Now the 
+  //reducer function has to cover this state in a conditional transition.
+  //See (B) in storiesReducer
   const handleRemoveStory = (item) => {
     dispatchStories({
       type: 'REMOVE_STORY',
@@ -128,6 +226,7 @@ const App = () => {
     });
   };
 
+  //Handler
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
